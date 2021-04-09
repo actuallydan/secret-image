8057363a2ed2419936a072321766b9ca121ea4c2
Dan Kral <dan.kral@clean.io>
2021-04-09 19:38:36 -0400

    spaced out chars for transparency jam

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,219 @@
-import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { useEffect, useState } from "react";
+import Head from "next/head";
 
 export default function Home() {
+  const [image, setImage] = useState(null)
+  const [imageUrl, setImageUrl] = useState(null)
+  const [text, setText] = useState("")
+  const [isHovering, setIsHovering] = useState(false);
+  const [bufferImage, setBufferImage] = useState(null);
+
+
+  useEffect(() => {
+    if (bufferImage) {
+      decode();
+    }
+  }, [bufferImage]);
+
+  function handleImageChange(e) {
+    const file = e.target.files[0];
+    updateImage(file)
+  };
+
+  async function updateImage(file) {
+    console.log(file)
+    setImage(file)
+    const url = URL.createObjectURL(file);
+    setImageUrl(url);
+
+    let blob = await fetch(url)
+      .then((r) => r.blob())
+
+    var buffer = await blob.arrayBuffer();
+    setBufferImage(buffer)
+  }
+
+  function updateText(e) {
+    setText(e.target.value)
+  }
+
+  function clearMem() {
+    URL.revokeObjectURL(imageUrl)
+  }
+
+  const handleDragEnter = e => {
+    e.preventDefault();
+    // e.stopPropagation();
+    console.log("change enter!")
+    setIsHovering(true)
+  };
+  const handleDragLeave = e => {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log("change leave!")
+    setIsHovering(false)
+  };
+
+  const handleDragOver = e => {
+    e.preventDefault();
+    e.stopPropagation();
+    setIsHovering(true)
+  };
+
+
+
+  const handleDrop = e => {
+    e.preventDefault();
+    e.stopPropagation();
+    const file = e.dataTransfer.files[0];
+    if (file.type === "image/png") {
+      updateImage(file)
+    }
+    setIsHovering(false)
+  };
+
+  function decode() {
+    // decode 
+    Jimp.read(bufferImage)
+      .then(image => {
+        let secret = [];
+
+        image.scan(0, 0, image.bitmap.width, image.bitmap.height, function (x, y, idx) {
+          // var alpha = this.bitmap.data[idx + 3];
+
+          secret.push(255 - this.bitmap.data[idx + 3]);
+
+        });
+        console.log(secret);
+        // convert string of numbers to characters
+        let triples = []
+        let latestSplice = "999"
+
+        while (latestSplice[0] === 0 && latestSplice[1] === 0 && latestSplice[2] === 0) {
+          latestSplice = secret.splice(0, 3);
+          if (latestSplice[0] === 0 && latestSplice[1] === 0 && latestSplice[2] === 0) {
+            triples.push(latestSplice)
+          }
+        }
+        console.log(triples)
+        secret = triples.map(t => String.fromCharCode(parseInt(t.join(""), 10)))
+
+        console.log(secret);
+
+        setText(secret)
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
+  function stringArrterator(arr) {
+    const str = arr;
+
+    return {
+      nextChar: () => {
+        return str.splice(0, 1)[0] || null
+      }
+    }
+
+  }
+  function encode() {
+
+    // do encoding
+    Jimp.read(bufferImage)
+      .then(image => {
+        // console.log(image.bitmap)
+        // convert text => char[] => int[] => char[] (number strings) => string[] (001, 032, 123) => char[][] => char[] (3x length of original arr)
+        let charArr = [...text].map(c => c.charCodeAt().toString().padStart(3, "0").split("")).flat();
+
+        const { nextChar } = stringArrterator(charArr);
+
+        for (let y = 0; y < image.bitmap.height; y++) {
+          for (let x = 0; x < image.bitmap.width; x++) {
+            const c = nextChar();
+            if (!c) {
+              break;
+            }
+            // create object with charCode alpha { r: 49, g: 58, b: 53, a: char.charCodeAt() }
+            const { r, g, b } = Jimp.intToRGBA(image.getPixelColor(x, y))
+            // convert rgba to hex with Jimp.rgbaToInt(r, g, b, a);
+            const newHex = Jimp.rgbaToInt(r, g, b, 255 - c)
+            // set pixel color as hex => image.setPixelColor(hex, x, y); 
+            image.setPixelColor(newHex, x, y);
+          }
+        }
+
+        return image.getBase64Async('image/png');
+      }).then(base64Image => {
+        // const blob = b64toBlob(base64Image);
+        // updateImage(newImage)
+        return blobToFile(base64Image);
+      }).then(file => {
+        updateImage(file);
+        setText("")
+        download(file)
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
+  function blobToFile(url) {
+    return (fetch(url)
+      .then(function (res) { return res.arrayBuffer(); })
+      .then(function (buf) { return new File([buf], image.name.replace(".png", "-modified.png"), { type: image.type }); })
+    );
+  }
+
+  function download(file) {
+    var element = document.createElement('a');
+    element.setAttribute('href', URL.createObjectURL(file));
+    element.setAttribute('download', file.name);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+  }
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <script src="https://unpkg.com/jimp@0.14.0/browser/lib/jimp.js"></script>
       </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+      <div className={styles.picAndTextWrapper}>
+        <fieldset className={styles.halfScreen}>
+          <legend>Text</legend>
+          <textarea value={text} className={styles.textarea} onChange={updateText} placeholder="Enter secret text to encode here!"></textarea>
+        </fieldset>
+        <fieldset className={styles.halfScreen}>
+          <legend>Image</legend>
+          <div onDrop={e => handleDrop(e)}
+            className={`${styles.dropTarget} ${isHovering ? styles.onHover : ""}`}
+            onDragOver={e => handleDragOver(e)}
+            onDragEnter={e => handleDragEnter(e)}
+            onDragLeave={e => handleDragLeave(e)}
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+            {image
+              ? <img src={imageUrl} onLoad={clearMem} className={styles.previewImage} />
+              : <div>
+                <label htmlFor="file-target" className={styles.button}>Click or Drop a File</label>
+                <input id="file-target"
+                  className={styles.hide}
+                  crossOrigin="true"
+                  type="file"
+                  accept="png"
+                  onChange={handleImageChange} />
+              </div>
+            }
+          </div>
+        </fieldset>
+      </div>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+      {bufferImage && text && <button onClick={encode}>Encode Text In Image</button>}
     </div>
   )
 }
