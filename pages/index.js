d6b1c87da215f3370d9e7c0877e6a8105f29e045
Dan Kral <dankral01@gmail.com>
2021-04-10 01:56:51 -0400

    way faster and looks much much better

--- pages/index.js
+++ pages/index.js
@@ -67,7 +67,6 @@ export default function Home() {
   };
 
   function decode() {
-    // decode 
     Jimp.read(bufferImage)
       .then(image => {
         let secret = [];
@@ -76,20 +75,15 @@ export default function Home() {
           // var alpha = this.bitmap.data[idx + 3];
 
           secret.push(255 - this.bitmap.data[idx + 3]);
-
         });
 
-        // convert string of numbers to characters
-        let triples = []
-        let latestSplice = [9, 9, 9]
+        const targetArr = new Array(secret.length / 3 | 0);
 
-        while (latestSplice[0] !== 0 || latestSplice[1] !== 0 || latestSplice[2] !== 0) {
-          latestSplice = secret.splice(0, 3);
-          if (latestSplice[0] !== 0 || latestSplice[1] !== 0 || latestSplice[2] !== 0) {
-            triples.push(latestSplice)
-          }
+        for (let i = 0; i < secret.length; i += 3) {
+          targetArr[i / 3] = secret[i] * 100 + secret[i + 1] * 10 + secret[i + 2]
         }
-        secret = triples.map(t => String.fromCharCode(parseInt(t.join(""), 10))).join("")
+
+        secret = targetArr.map(t => String.fromCharCode(t)).join("")
 
         setText(secret)
       })
@@ -113,26 +107,34 @@ export default function Home() {
     // do encoding
     Jimp.read(bufferImage)
       .then(image => {
-        // console.log(image.bitmap)
         // convert text => char[] => int[] => char[] (number strings) => string[] (001, 032, 123) => char[][] => char[] (3x length of original arr)
+        let t0 = performance.now();
         let charArr = [...text].map(c => c.charCodeAt().toString().padStart(3, "0").split("")).flat();
-        console.log(charArr)
-        const { nextChar } = stringArrterator(charArr);
-
+        // const { nextChar } = stringArrterator(charArr);
+        let c = 0
         for (let y = 0; y < image.bitmap.height; y++) {
           for (let x = 0; x < image.bitmap.width; x++) {
-            const c = nextChar();
+            c = charArr[x + y * image.bitmap.width];
+
             if (!c) {
               break;
             }
+
             // create object with charCode alpha { r: 49, g: 58, b: 53, a: char.charCodeAt() }
             const { r, g, b } = Jimp.intToRGBA(image.getPixelColor(x, y))
             // convert rgba to hex with Jimp.rgbaToInt(r, g, b, a);
             const newHex = Jimp.rgbaToInt(r, g, b, 255 - c)
+            // let t2 = performance.now();
+
+            // console.log("second part", t2 - t1);
             // set pixel color as hex => image.setPixelColor(hex, x, y); 
             image.setPixelColor(newHex, x, y);
+            // console.log("third part", performance.now() - t2);
+
           }
         }
+        let t1 = performance.now();
+        console.log("first part", t1 - t0);
 
         return image.getBase64Async('image/png');
       }).then(base64Image => {
@@ -169,42 +171,53 @@ export default function Home() {
     document.body.removeChild(element);
   }
 
+  const disableEncode = !bufferImage || !text;
+  const disableDecode = !image || !imageUrl || !bufferImage;
+
   return (
     <div className={styles.container}>
       <Head>
         <script src="https://unpkg.com/jimp@0.14.0/browser/lib/jimp.js"></script>
       </Head>
       <div className={styles.picAndTextWrapper}>
-        <fieldset className={styles.halfScreen}>
-          <legend>Text</legend>
-          <textarea value={text} className={styles.textarea} onChange={updateText} placeholder="Enter secret text to encode here!" ></textarea>
-          {bufferImage && text && <button onClick={encode}>Encode Text In Image</button>}
+        <div className={styles.halfScreen}>
+          <fieldset className={styles.fieldset}>
+            <legend>Text</legend>
+            <textarea value={text} className={styles.textarea} onChange={updateText} placeholder="Enter secret text to encode here!" ></textarea>
 
-        </fieldset>
-        <fieldset className={styles.halfScreen}>
-          <legend>Image</legend>
-          <div onDrop={e => handleDrop(e)}
-            className={`${styles.dropTarget} ${isHovering ? styles.onHover : ""}`}
-            onDragOver={e => handleDragOver(e)}
-            onDragEnter={e => handleDragEnter(e)}
-            onDragLeave={e => handleDragLeave(e)}
-          >
-            {image
-              ? <img src={imageUrl} onLoad={clearMem} className={styles.previewImage} />
-              : <div>
-                <label htmlFor="file-target" className={styles.button}>Click or Drop a File</label>
-                <input id="file-target"
-                  className={styles.hide}
-                  crossOrigin="true"
-                  type="file"
-                  accept="png"
-                  onChange={handleImageChange} />
-              </div>
-            }
-          </div>
-          {image && imageUrl && bufferImage && <button onClick={decode}>Decode Text From Image</button>}
+          </fieldset>
+          <button disabled={disableEncode} className={`${styles.button} ${disableEncode ? styles.disableBtn : ""}`} onClick={encode}>
+            Encode Text
+          </button>
+        </div>
+        <div className={styles.halfScreen}>
+          <fieldset className={styles.fieldset}>
+            <legend>Image</legend>
+            <div onDrop={e => handleDrop(e)}
+              className={`${styles.dropTarget} ${isHovering ? styles.onHover : ""}`}
+              onDragOver={e => handleDragOver(e)}
+              onDragEnter={e => handleDragEnter(e)}
+              onDragLeave={e => handleDragLeave(e)}
+            >
+              {image
+                ? <img src={imageUrl} onLoad={clearMem} className={styles.previewImage} />
+                : <div>
+                  <label htmlFor="file-target" className={styles.button}>Click or Drop a File</label>
+                  <input id="file-target"
+                    className={styles.hide}
+                    crossOrigin="true"
+                    type="file"
+                    accept="png"
+                    onChange={handleImageChange} />
+                </div>
+              }
+            </div>
 
-        </fieldset>
+          </fieldset>
+          <button disabled={disableDecode} className={`${styles.button} ${disableDecode ? styles.disableBtn : ""}`} onClick={decode}>
+            Decode From Image
+          </button>
+        </div>
       </div>
 
     </div>
